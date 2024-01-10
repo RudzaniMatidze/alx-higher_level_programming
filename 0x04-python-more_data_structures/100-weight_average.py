49df1b4011ab22ac28982dad4373a525817d3afa
RudzaniMatidze <rdzmatidze@gmail.com>
2024-01-10 11:05:41 -0800

    Write a function that returns the weighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
+    if not isinstance(my_list, list) or len(my_list) == 0:
         return 0
-    total_score = 0
-    total_weight = 0
 
-    for score, weight in my_list:
-        total_score += score * weight
-        total_weight += weight
+    average = 0
+    size = 0
+    for tup in my_list:
+        average += (tup[0] * tup[1])
+        size += tup[1]
 
-        Average = total_score /total_weight
-        return Average
+    return (average / size)
