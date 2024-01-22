7d606f61000ec2a398548b8834ff110e9ba1cdd1
RudzaniMatidze <rdzmatidze@gmail.com>
2024-01-22 08:49:42 -0800

    Write a function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-        a = 0
-        count = 0
-        for a in range(0, x):
-            try:
-                print("{}".format(my_list[a]), end="")
-                count += 1
-            except:
-                continue
-            print()
-            return count
+    try:
+        s_print = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        print()
+        return s_print
+    except IndexError:
+        print()
+        return s_print
