469913caa31fd36ba11a16485990acad20137c28
RudzaniMatidze <rdzmatidze@gmail.com>
2024-01-22 12:59:16 -0800

    Python function def magic_calculation(a, b):

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for k in range(1, 3):
+        try:
+            if k > a:
+                raise Exception('Too far')
+            result += a ** b / k
+        except Exception:
+            result = b + a
+            break
+    return result
