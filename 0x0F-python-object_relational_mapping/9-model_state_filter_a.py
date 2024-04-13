6e37fe77c7baed51354bb32232576e95bd452844
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 05:07:42 -0700

    file updated

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""Module that retrieves and prints the states with
-	letter a from a MySQL database using SQLAlchemy."""
-
+"""Module that retrieves and prints the states with\
+        letter a from a MySQL database using SQLAlchemy."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Creates the SQLAlchemy engine using the provided MySQL credentials
-    mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    engine = create_engine(mysql)
-    Base.metadata.create_all(engine)
-    #Creates a session factory
+    # Create a session factory
     Session = sessionmaker(bind=engine)
 
-    # Creates a session object
+    # Create a session object
     session = Session()
 
-    # Retrieves the states with letter 'a' from the
+    # Retrieve the states with letter 'a' from the
     # database and print its ID and name
     for state in session.query(State).order_by(State.id):
         if "a" in state.name:
