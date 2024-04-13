411727e08682526863ecc1b0983522d041e5acd4
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 11:27:19 -0700

    file updated

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,10 +5,19 @@ from model_state import Base
 
 
 class City(Base):
-    """Represents acity for a MySql database.
+    """Represents a city for a MySql database.
+
+    Attributes:
+        __tablename__ (str): name of table
+        id: Represents a column of an auto_generated, unique integer,
+            can't be null and is a primary key
+        name: Represents a column of a string with maximum 128 char and
+            can't be null
+        state_id: Represents a of a ForeignKey named states.id
 
     """
     __tablename__ = 'cities'
+
     id = column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
