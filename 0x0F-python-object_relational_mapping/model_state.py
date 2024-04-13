5b9c7e26cd93e19fa8f95f1dacd7a37af7128f48
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 01:50:37 -0700

    file updated

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """Starts link class to table in database."""
 
-import sys
-from sqlalchemy import Column, Integer, String, Metadata
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Class representing the states table."""
+    """Class representing the states table
+
+    Attributes:
+    __tablename__ (str): name of the table
+    id: Represents a column of a auto-generated, unique integer
+        can't be null and a is primary key
+    name: Represents a column of string with maximum 128 char and
+        can't be null.
+    """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
