5ca797b6fb733469eb68a007e8e89edc0a6d6ae9
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 12:47:41 -0700

    file updated

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -8,7 +8,16 @@ from relationship_state import Base, State
 
 
 class City(Base):
-    """City class """
+    """class definition of a state
+
+    Attributes:
+        __tablename__ (str): name of the table
+        id: Represents a column of an auto-generated, unique integer,
+            can't be null and is a primary key
+        name: Represents a column of a string with maximum 128 characters
+            and can'tbe null
+        state.id: Represents a column of string id
+    """
 
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False)
