b81e3ce2974af6143ab31504c48d91cfcbde7d99
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 05:22:26 -0700

    file updated

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Creates the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost3306/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
