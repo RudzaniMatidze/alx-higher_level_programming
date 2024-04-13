edef926a0bb0b6de0f6df9855d3ce8407cc18e78
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 11:52:52 -0700

    file updated

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     # Creates the SQLAlchemy engine using the provided MySQL credentials
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
