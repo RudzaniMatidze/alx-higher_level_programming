7d15b98d9cff6c3f0737a74d9dbcf2e524a79776
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-12 18:20:43 -0700

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,21 @@
-#!/usr/bin/pyhton3
-"""Module that lists all states from the MySQL server"""
+#!/usr/bin/python3
+
+"""Module that lists all states from the hbtn_0e_0_usa database."""
 
 import sys
 import MySQLdb
 
-if __name__ = '__main__':
+if __name__ == "__main__":
 
-    #Get MySQL credentials and search the name from command-line args
+    # Get MySQL credentials and search name from command-line arguments
+    # and # Connect to MySQL server
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
 
-    #Execute the SQL query to retrieve states with the specified name
+    # Execute the SQL query to retrieve states with the specified name
     c.execute("SELECT * \
-            FROM `states` \
-            WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
 
-    #Fetch all rows and print the states
+    # Fetch all rows and print the states
     [print(state) for state in c.fetchall()]
