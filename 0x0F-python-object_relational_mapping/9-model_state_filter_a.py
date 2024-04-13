fd415a96f8c3cffd5edaf79734ce1ea4d8272a69
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 04:57:57 -0700

    file updated

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,12 +9,14 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Creates the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    mysql = 'mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
     # Creates a session factory
     Session = sessionmaker(bind=engine)
+    engine = create_engine(mysql)
+    Base.metadata.create_all(engine)
 
     # Creates a session object
     session = Session()
