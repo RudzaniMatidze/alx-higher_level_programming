c772b0d06534996fb338cbd23ff076e10e160c2b
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 04:40:44 -0700

    file updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,10 @@
 import sys
 import MySQLdb
 
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
 def list_states(username, password, database):
     """lists all states from the database hbtn_0e_0_usa.
@@ -13,11 +17,14 @@ def list_states(username, password, database):
     """
 
     # Connects to the MySQL server
-    db = MySQLdb.connect(host='localhost',
+    db = MySQLdb.connect(
+            host="localhost",
             port=3306,
             user=username,
             passwd=password,
             db=database)
+
+    # Creates a cursor object using cursor() method
     cursor = db.cursor()
 
     # Executes the SQL query to fetch all states
