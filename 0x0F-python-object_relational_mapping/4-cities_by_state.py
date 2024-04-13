6c86a55a0c47926f2d61220848fdeb73521bb19d
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-13 00:24:48 -0700

    comment errors fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,19 +4,20 @@
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    #Get MySQL credentials and search name from command-line arguments
+    # Gets MySQL credentials and search name from command-line arguments
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    #Connect to MySQL server
+    # Connects to MySQL server
     c = db.cursor()
 
-    #Execute the SQL query to retrieve all states
+    # Executes the SQL query to retrieve all states
     c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
                  FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
 
-    #Fetch all rows and print the states
+    # Fetches all rows and print the cities
     [print(city) for city in c.fetchall()]
