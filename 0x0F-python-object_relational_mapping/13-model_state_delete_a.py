0c13e7ac1c6074691be8d565c1ba3217ccaaad04
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-14 06:43:21 -0700

    file updated

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -8,21 +8,13 @@ from model_state import State
 
 
 if __name__ == "__main__":
-    # Creates the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # Creates a session factory
     Session = sessionmaker(bind=engine)
-    # Creates a session object
     session = Session()
 
-    # Retrieves all states from the database
     for state in session.query(State):
-        # Checks if the state's name contains the letter "a"
         if "a" in state.name:
-            # Deletes the state from the session
             session.delete(state)
-    # Commits the session to persist the changes
     session.commit()
