3ac199e5686ce260bebf222fb16e04a6adc91be3
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-14 08:13:23 -0700

    file updated

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,14 +2,16 @@
 """Module that retrieves and prints a list of cities with their
         associated states from a MySQL database using SQLAlchemy.
 """
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
-from model_city import City
-from sqlalchemy.schma import Table
 
 if __name__ == "__main__":
+
+    import sys
+    from model_city import City
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
     # Creates the SQLAlchemy engine using the provided MySQL credentials
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -20,6 +20,6 @@ class City(Base):
 
     """
     __tablename__ = 'cities'
-    id = column(Integer, primary_key=True, nullable=False)
+    id = column(Integer, primary_key=True, nullable=False,)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
