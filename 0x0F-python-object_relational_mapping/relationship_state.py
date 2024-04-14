7dc9d6d606d5ae2143ab92e84763bca937d81947
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-14 07:19:37 -0700

    file updated

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -8,6 +8,8 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+Base = declarative_base()
+
 
 class State(Base):
     """Represents a State for a MySQL database.
