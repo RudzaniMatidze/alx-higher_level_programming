666ddd4d4045126096107e3f01f74bb0c342ff6f
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-14 08:17:49 -0700

    file updated

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,11 +9,9 @@ if __name__ == "__main__":
     from sqlalchemy.orm import Session
     from sqlalchemy.schema import Table
 
-    #Create the SQLAchemy engine using the provided MySQL credentials
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-    # Create engine metadata
     Base.metadata.create_all(engine)
 
     session = Session(engine)
