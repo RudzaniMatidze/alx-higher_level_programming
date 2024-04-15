4d016b5273e7d75863e41e44cffd7af3d15c24c4
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-15 04:36:17 -0700

    file updated

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""Module that retrieves and prints a list of cities with their
-        associated states from a MySQL database using SQLAlchemy.
-"""
+"""prints all City objects
+from the database hbtn_0e_14_usa"""
 
 if __name__ == "__main__":
 
     import sys
-    from model_city import City
     from model_state import Base, State
+    from model_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
     from sqlalchemy.schema import Table
 
-    # Creates the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    # Creates a session object
-    session = Session()
-
-    # Retrieves cities and their associated states from the database
-    # by joining the City and State tables based on the state_id
-    # and ordering the results by city ID
-    for city, state in session.query(City, State)\
+    session = Session(engine)
+    for state, city in session.query(State, City)\
                               .filter(City.state_id == State.id)\
                               .order_by(City.id).all():
-        # Print the city and state information
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
