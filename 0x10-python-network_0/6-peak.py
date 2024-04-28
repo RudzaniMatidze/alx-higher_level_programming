e04897c80a4ff6f1038826ae6d904a9563750445
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-28 09:39:01 -0700

    file updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Finds a peak in unsorted integer list"""
+"""Find a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds peak in list_of_integers"""
+    """Find a peak in list_of_integers"""
 
     if list_of_integers is None or list_of_integers == []:
         return None
@@ -13,12 +13,12 @@ def find_peak(list_of_integers):
     mid = int(mid)
     if hi == 1:
         return list_of_integers[0]
-    elif hi == 2:
+    if hi == 2:
         return max(list_of_integers)
-    elif list_of_integers[mid] >= list_of_integers[mid - 1] and\
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
             list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
-    elif mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid:])
-    elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
