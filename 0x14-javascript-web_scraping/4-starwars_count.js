56b4d8153a339e4b47ce9842286e80b506aa8ee2
RudzaniMatidze <rdzmatidze@gmail.com>
2024-05-21 13:04:21 -0700

    file updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,23 @@
 #!/usr/bin/node
-// Number of movies where the character “Wedge Antilles” 
+// Number of films with the given character ID
 
 const request = require('request');
-let num = 0;
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-request.get(process.argv[2], (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const content = JSON.parse(body);
-    content.results.forEach((film) => {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
       film.characters.forEach((character) => {
-        if (character.includes(18)) {
-          num += 1;
+        if (character.includes(characterId)) {
+          count += 1;
         }
       });
     });
-    console.log(num);
+    console.log(count);
   }
 });
